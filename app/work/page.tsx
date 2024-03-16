1792d43989ebdf25968e01a870294e40d20d473d
Shounak Rangwala <shounak.rangwala@prodigiq.com>
2024-03-15 17:22:56 -0700

    Added the content for work page

--- app/work/page.tsx
+++ app/work/page.tsx
@@ -1,7 +1,19 @@
-export default function Work(){
+import DisplayCard from "../components/DisplayCard/displaycard"
+import { CardData } from "../lib/interfaces"
+import { work } from "@/content/work"
+
+export default function Education(){
   return(
     <div className="p-4">
-      <p>Work</p>
+      <ul>
+        {work.map((work, index) => {
+          return(
+            <li className="mb-12" key={index}>
+              <DisplayCard props={work as CardData}/>
+            </li>
+          )
+        })}
+      </ul>
     </div>
   )
 }
---
+++ content/work.tsx
@@ -0,0 +1,87 @@
+export const work = [
+  {
+    sidebar: 'Jun 2021 - Present',
+    header: 'Software Engineer · ProDIGIQ Inc.',
+    body:'Designed and refactored the Enterprise suite software optimizing the Gate Management and the Lease Management Systems to process higher magnitude of Data. Took point in integrating current systems with third-party payment portals like Peoplesoft and Chase Orbital. Mentored junior developers on best practices and was in charge of maintaining and code reviewing the Enterprise suite repositories.',
+    link: undefined,
+    skills: {
+      skills:
+        [
+          {
+            name: 'Ruby on Rails',
+            color: 'lime'
+          },
+          {
+            name: 'jQuery',
+            color: 'emerald'
+          },
+          {
+            name: 'VueJS',
+            color: 'slate'
+          },
+          {
+            name: 'RSpec',
+            color: 'slate'
+          },
+          {
+            name: 'Git',
+            color: 'slate'
+          },
+          {
+            name: 'MySQL',
+            color: 'slate'
+          }
+        ]
+    }
+  },
+  {
+    sidebar: 'Jan 2019 - Jun 2019',
+    header: 'Self-Driving Intern · WINLAB',
+    body:'Designed and implemented a CNN using Robot operating system and simulated the tests using Gazebo. The result was a self-driving car model whic could drive around the simulated environment based on the camera input being processed through a neural network. ',
+    link: undefined,
+    skills: {
+      skills:
+        [
+          {
+            name: 'ROS',
+            color: 'lime'
+          },
+          {
+            name: 'Gazebo',
+            color: 'emerald'
+          },
+          {
+            name: 'Python',
+            color: 'slate'
+          },
+          {
+            name: 'NumPy',
+            color: 'slate'
+          }
+        ]
+    }
+  },
+  {
+    sidebar: 'Jan 2019 - Jun 2019',
+    header: 'Software Development Intern · HERE Technologies',
+    body:'Developed a web-application (as part of an Agile team) for rendering 3D structures for geospatial visualizations using Ubers open-source graphics library Deck.gl. The application could show accurate models of roads, poles, barriers and lanes in any part of the planet. Enabled a THree.js camera to traverse through a road on the visualization with adjustable speed.',
+    link: undefined,
+    skills: {
+      skills:
+        [
+          {
+            name: 'Javascript',
+            color: 'lime'
+          },
+          {
+            name: 'Deck.GL',
+            color: 'emerald'
+          },
+          {
+            name: 'ThreeJS',
+            color: 'slate'
+          }
+        ]
+    }
+  },
+]
